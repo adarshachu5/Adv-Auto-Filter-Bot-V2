6f2db37ba7bef01b11c8ad6724661c5e76bac6e6
adarshachu5 <83229823+adarshachu5@users.noreply.github.com>
2021-07-09 14:17:19 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -23,7 +23,7 @@ async def start(bot, update):
         if (file_id or file_type) == None:
             return
         
-        caption = file_caption if file_caption != ("" or None) else ("<code>" + file_name + "</code>" അടിയിൽ)
+        caption = file_caption if file_caption != ("" or None) else ("<code>" + file_name + "</code>")
         
         if file_type == "document":
         
